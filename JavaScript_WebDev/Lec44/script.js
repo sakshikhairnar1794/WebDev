dceda83e420af0722b8e47b35d6b4970dcbd96a2
sakshikhairnar1794 <sakshikhairnar40@gmail.com>
2026-03-04 16:42:33 +0530

    Updated

--- JavaScript_WebDev/Lec44/script.js
+++ JavaScript_WebDev/Lec44/script.js
@@ -71,3 +71,76 @@ let filteredValue = arr.filter((value) =>{
 })
 console.log(filteredValue);  
 
+let err = [10, 20, 30, 90, 50];
+
+let ans = err.reduce((acc, curr)=> {
+    return acc+curr;
+}, 0)
+
+console.log(ans);
+
+console.log(err.sort()) ;
+
+console.log(err);
+console.log(err.indexOf(90));
+
+//for each
+let length = err.length;
+console.log(length);
+err.forEach((value, index) =>{
+    console.log("Number: ", value, "Index: ", index);
+})
+
+//traditional for loop
+
+for(let i=0; i<length; i++){
+    console.log(err[i])
+}
+
+// for -in loop to iterate on innumerable 
+
+for(let key in obj){
+    console.log(key, " ", obj[key]);
+}
+
+//for -of loop
+
+let fullName = "SAkshi";
+for(let val of fullName){
+    console.log(val);
+}
+
+
+//Arrays with function
+let grr = [10,20,30,40,50];
+function getSum(grr){
+    let len = grr.length;
+    let sum = 0;
+    for(let i = 0; i<len; i++){
+        sum = sum + grr[i];
+    }
+    return sum;
+}
+let totalSum = getSum(grr);
+console.log(totalSum);
+
+
+// function getSum(arr){
+//     let sum = 0;
+//     arr.forEach((value) =>{
+//         sum = sum + value;
+//     })
+//     return sum;
+// }
+// console.log(getSum(arr));
+
+// can be written as
+
+let getsum = (arr)=>{
+    let sum = 0;
+    arr.forEach((value) =>{
+        sum = sum + value;
+    })
+    return sum;
+}
+console.log(getsum(arr));
